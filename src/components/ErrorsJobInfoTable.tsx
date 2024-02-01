72a4f2cc8822d24fad73efa2ef3850283a95bd3c
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2024-02-01 11:30:11 -0800

    added key to tables

--- src/components/ErrorsJobInfoTable.tsx
+++ src/components/ErrorsJobInfoTable.tsx
@@ -19,14 +19,14 @@ export const ErrorsJobInfoTable = (): JSX.Element => {
                 {ERRORS_JOBS_INFO.map((field) => {
                     {
                         if (selectedJob['jobInfo'][field.accessor]) {
-                            return <tr>
+                            return <tr key={field.header}>
                                 <th>{field.header}</th>
                                 <td>
                                     <div onClick={() => setExpandError(!expandError)} className={expandError ? "show-content clickable" : "hide-content clickable"}>{expandError ? <MdArrowDropDown size={28} /> : <MdArrowRight size={28} />}{selectedJob['jobInfo'][field.accessor]}</div>
                                 </td>
                             </tr>
                         } else {
-                            return <tr>
+                            return <tr key={field.header}>
                                 <th>{field.header}</th>
                                 <td>{EMPTY_FIELD_CHAR}</td>
                             </tr>
