4b026fb00b09151b305e3dde4bdbcf85a63223c3
Marjorie Lucas <marjorie.j.lucas@jpl.nasa.gov>
2026-05-05 11:49:07 -0700

    fix

--- src/utils/generic.ts
+++ src/utils/generic.ts
@@ -145,7 +145,8 @@ export const getOutputWorkspacePath = (output: Record<string, unknown>[]): strin
   if (s3Key.includes('dps_output')) {
     return 'my-private-bucket/' + s3Key.slice(s3Key.indexOf('dps_output'));
   } else if (s3Key.includes('triaged_job')) {
-    return s3Key.slice(s3Key.indexOf('triaged_job'));
+    // mounted path uses 'triaged-jobs' instead of 'triaged_job'
+    return 'triaged-jobs/' + s3Key.slice(s3Key.indexOf('triaged_job') - 1);
   } else {
     console.warn('Unexpected S3 key does not map to dps_output or triaged_job: ', s3Key);
     return null;
