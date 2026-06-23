317c246b78a3ac9a28ca70fd1f29e441b17af206
Marjorie Lucas <marjorie.j.lucas@jpl.nasa.gov>
2026-06-23 13:58:04 -0700

    implemented cancel feature | cleaned up constants

--- src/constants.ts
+++ src/constants.ts
@@ -18,39 +18,16 @@ export const JUPYTER_EXT = {
 };
 
 /*******************************
- * Jobs Tables
- *******************************/
-
-export const MAX_CELL_WIDTH = 350;
-
-export const STYLE_TYPE = {
-  CODE: 'code',
-  TEXT: 'text',
-  URL: 'url',
-};
-
-export const EMPTY_FIELD_CHAR = '-';
-
-export const SUBMITTING_JOB_TEXT = 'Submitting job...';
-export const SUBMITTED_JOB_SUCCESS = '{TIME}\nJob submitted successfully. {ID}';
-export const SUBMITTED_JOB_FAIL = '{TIME}\nJob submission failed because {ERROR}';
-export const SUBMITTED_JOB_ELEMENT_ID = 'submitting_job_text';
-
-export const JOB_STARTED = 'job-started';
-export const JOB_QUEUED = 'job-queued';
-
-/*******************************
  * MAAP API ENDPOINTS
  *******************************/
 export const MAAP_API_ENDPOINTS = {
-  SUBMIT_JOB: 'api/ogc/processes/{PROCESS_ID}/execution',
-  GET_JOBS: 'api/ogc/jobs',
-  GET_JOB_BY_ID: 'api/ogc/jobs/{JOB_ID}',
-  GET_JOB_RESULTS: 'api/ogc/jobs/{JOB_ID}/results',
-  GET_PROCESSES: 'api/ogc/processes',
-  GET_PROCESS: 'api/ogc/processes/{PROCESS_ID}',
-  GET_RESOURCES: 'api/mas/algorithm/resource',
-  CANCEL_EXECUTION: 'api/dps/job/cancel/{JOB_ID}',
+  JOBS: 'api/ogc/jobs',
+  JOBS_JOBID: 'api/ogc/jobs/{JOB_ID}',
+  JOBS_JOBID_RESULTS: 'api/ogc/jobs/{JOB_ID}/results',
+  PROCESSES: 'api/ogc/processes',
+  PROCESSES_PROCESSID: 'api/ogc/processes/{PROCESS_ID}',
+  PROCESSES_PROCESSID_EXECUTION: 'api/ogc/processes/{PROCESS_ID}/execution',
+  ALGORITHM_RESOURCE: 'api/mas/algorithm/resource',
 };
 
 /*******************************
